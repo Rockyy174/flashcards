94232054732fc462582fda4f4f8504d9c4f2ac9a
Macved <macved@Macveds-MacBook-Air.local>
2022-03-15 22:17:23 +0500

    final adjustments

--- website/settings.py
+++ website/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config.get('CS50_FLASHCARDS_DJANGO_KEY')
 
 # SECURIfTY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
